277fd0fee4fa8acc4f6bbfe653f4592c2fae9b40
Anderson Ramos <125606509+anderson-rdev@users.noreply.github.com>
2025-04-23 01:55:02 -0300

    Create beecrowd_basic_1001.py
    Resolução de problema do "beecrowd" - atividade 1001 !

---
+++ beecrowd_basic_1001.py
@@ -0,0 +1,32 @@
+# Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+# Entrada
+# A entrada contém 2 valores inteiros.
+
+# Saída
+# Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+
+# Exemplo:
+===========================================
+| Exemplos de Entrada | Exemplos de Saída |  
+|---------------------|------------------ |  
+| 10   e  9           | X = 19            |  
+| –10  e  4           | X = –6            |  
+| 15   e –7           | X = 8             |
+===========================================
+
+#Criando Função
+def somando_ab(a, b):
+    # Somando A e B
+    x = a + b
+    return x
+    
+# Leitura de Dados
+a = int(input(''))  
+b = int(input(''))        
+
+# Chamando função
+somando = somando_ab(a, b)
+
+# Imprimindo resultado final
+print(f'X = {somando}')
