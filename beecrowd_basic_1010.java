ae4344dd0681bf8fa704d45443c2eb45d4eb2fe4
Anderson Ramos <125606509+anderson-rdev@users.noreply.github.com>
2025-08-02 21:10:28 -0300

    refactor(core): aplicando orientação a objetos

--- beecrowd_basic_1010.java
+++ beecrowd_basic_1010.java
@@ -1,23 +1,46 @@
+package calculosimples;
 import java.util.Scanner;
 import java.util.Locale;
 
-public class Main {
+class Peca {
+    private int codigo;
+    private int quantidade;
+    private double valorUnitario;
+    
+    public Peca(int codigo, int quantidade, String valorStr){
+        this.codigo = codigo;
+        this.quantidade = quantidade;
+        
+        // Substituindo vírgula pelo e convertendo para double
+        this.valorUnitario = Double.parseDouble(valorStr.replace(',','.'));
+    }
+    
+    public double getSubtotal(){
+        return quantidade * valorUnitario;
+    }
+}
+
+public class CalculoSimples {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int peca1 = sc.nextInt();
-        int numero1 = sc.nextInt();
+        
+        // Leitura de dados peça 1
+        int codigo1 = sc.nextInt();
+        int quantidade1 = sc.nextInt();
         String valorStr1 = sc.next();
-        valorStr1 = valorStr1.replace(',', '.');
-        double valorunitario1 = Double.parseDouble(valorStr1);
 
-        int peca2 = sc.nextInt();
-        int numero2 = sc.nextInt();
+        // Leitura de dados peça 1
+        int codigo2 = sc.nextInt();
+        int quantidade2 = sc.nextInt();
         String valorStr2 = sc.next();
         valorStr2 = valorStr2.replace(',', '.');
-        double valorunitario2 = Double.parseDouble(valorStr2);
-
-        double total = (numero1 * valorunitario1) + (numero2 * valorunitario2);
+        
+        // Criando objetos para as peças 
+        Peca peca1 = new Peca(codigo1,quantidade1,valorStr1);
+        Peca peca2 = new Peca(codigo2,quantidade2,valorStr2);
+        
+        // Calculando o total 
+        double total = peca1.getSubtotal() + peca2.getSubtotal();
 
         System.out.printf(Locale.US,"VALOR A PAGAR: R$ %.2f\n", total);
 
